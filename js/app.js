a9016af10ed7e4315a92d5dae39b9f224614d482
Willy Aguirre <marti1125@gmail.com>
2013-10-03 21:43:12 -0500

    save image and change icons launch v2

--- js/app.js
+++ js/app.js
@@ -1,35 +1,44 @@
+function randomUUID() { 
+	var s = [], itoh = '0123456789ABCDEF';
+	for (var i = 0; i <36; i++) s[i] = Math.floor(Math.random()*0x10);
+	s[14] = 4;
+	s[19] = (s[19] & 0x3) | 0x8;
+	for (var i = 0; i <36; i++) s[i] = itoh[s[i]];
+	s[8] = s[13] = s[18] = s[23] = '_';
+	return s.join(''); 
+}
+
 $(document).on('ready',function(){
 	var myBoard = new DrawingBoard.Board('board');
-	myBoard.addControl('Download');
+	//myBoard.addControl('Download');
 	
-	$(".drawing-board-control-download-button").click(function(){
+	$(".drawing-board-control-save-button").click(function(){
+		var randomImage = randomUUID();
 		var cnv = document.getElementById("drawing-board-id");
 		cnv.toBlob(function (blob) {		
 		
-			var sdcard = navigator.getDeviceStorage("pictures");
-			var request = sdcard.delete("mycddddandddvas.png");
+			var pic = navigator.getDeviceStorage("pictures");
+			var request = pic.delete("abc.png");
 			
 			request.onsuccess = function () {
 				saveAndSend(blob);
 			}
 
-				function saveAndSend(blob) {				
-					var storage = navigator.getDeviceStorage("pictures");
-					var request = storage.addNamed(blob, "mycddddandddvas.png");
+			function saveAndSend(blob) {				
+				var storage = navigator.getDeviceStorage("pictures");
+				var request = storage.addNamed(blob, "image-"+randomImage+".png");
 
-					request.onsuccess = function () {
-						alert('save image succesful')						
-					}
-					
-					request.onerror = function () {
-						alert('can not save')
-					}
-					
+				request.onsuccess = function () {
+					alert('the image has saved successfully')						
 				}
-					
+				
+				request.onerror = function () {
+					alert('can not save the image')
+				}
+				
+			}					
 		
-		}); 
-		
+		}); 		
 		
 	});
 	
